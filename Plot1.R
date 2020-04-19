e37dccb0cfc42a2e1cc5a6d907007d107b16fb2c
Sebastien GAUTHIER <61781017+timouth14@users.noreply.github.com>
2020-04-19 13:02:44 +0200

    Plot1 code and Png

---
+++ Plot1.R
@@ -0,0 +1,14 @@
+## download file manually in the folder Exploratory-Data-Analisys
+## Create a table
+Power <- read.table("household_power_consumption.txt", sep = ";", header= T)
+
+Power$Date <- strptime(Power$Date,"%d/%m/%Y")
+
+##subsetting the date 01/02/2007 to 02/02/2007
+
+Power2 <- subset(Power, Power$Date >= as.POSIXct('2007-02-01') & Power$Date <= as.POSIXct('2007-02-02'))
+
+Power2$Global_active_power <- as.numeric(as.character(Power2$Global_active_power))
+hist(Power2$Global_active_power, col ="red", xlab = "Global Active Power (kilowatts)", main = "Global Active Power" )
+dev.print(device = png,file = "plot1.png", width = 480, height = 480)
+dev.off()
