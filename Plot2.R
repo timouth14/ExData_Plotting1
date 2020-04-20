9c127ad7c14c7e8b0e428bdfe1a2f8a4a035c939
Sebastien GAUTHIER <61781017+timouth14@users.noreply.github.com>
2020-04-20 12:33:25 +0200

    Plot2 code and png

---
+++ Plot2.R
@@ -0,0 +1,24 @@
+## download file manually in the folder Exploratory-Data-Analisys
+## Create a table
+Power <- read.table("household_power_consumption.txt", sep = ";", header= T, na.strings = "?")
+
+Power$Date <- strptime(Power$Date,"%d/%m/%Y")
+
+##df$DateTime <- strptime(paste(df$Date, df$Time), format="%d/%m/%Y %H:%M:%S")
+
+
+
+##subsetting the date 01/02/2007 to 02/02/2007
+
+Power2 <- subset(Power, Power$Date >= as.POSIXct('2007-02-01') & Power$Date < as.POSIXct('2007-02-03'))
+
+Power2$Date_Time <- paste(Power2$Date, Power2$Time)
+
+Power2$Date_Time <- as.POSIXct(Power2$Date_Time)
+
+plot(Power2$Global_active_power~Power2$Date_Time, type ="l", ylab = "Global Active Power (kilowatts", xlab = "")
+
+dev.print(device = png,file = "plot2.png", width = 480, height = 480)
+
+dev.off()
+
